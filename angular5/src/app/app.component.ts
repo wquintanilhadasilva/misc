b17310530f0a49677375252e0dd567980fc9d0fd
Wedson Quintanilha <wquintanilhadasilva@gmail.com>
2018-05-01 23:07:08 -0300

    teste

--- angular5/src/app/app.component.ts
+++ angular5/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  dados: any = { prop: { list: [3, 2, 1, 0] } };
+
+  array: any[] = [{ name: 'John'} , { name: 'Mary' }, { name: 'Adam' }];
+  order: string = 'name';
+
+  asc = false;
+
+  constructor() {}
+
+  setOrder() {
+    this.asc = !this.asc;
+  }
+
 }
--- angular5/src/app/app.module.ts
+++ angular5/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { PipesModule } from 'w-ng5';
+import { OrderModule } from 'ngx-order-pipe';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    PipesModule,
+    OrderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
